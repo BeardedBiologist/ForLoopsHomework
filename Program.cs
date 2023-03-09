8c66a01d3b81d3d60fe22b11761d2c3f28265439
BeardedBiologist <46723997+BeardedBiologist@users.noreply.github.com>
2023-03-09 20:29:22 +0100

    Final commit

--- Program.cs
+++ Program.cs
@@ -7,11 +7,15 @@
  */
 
 // 1. Ask user for : comma-separated list of first names (no spaces)
-
+Console.Write("Please write comma-separated list of first names (no spaces)");
+string? firstNamesStr = Console.ReadLine();
 
 // 2. Split to spring array
-
+string[] firstNames = firstNamesStr.Split(',');
 
 // 3. Loop through array and print hello name to console for each person
-
+for (int i = 0; i < firstNames.Count(); i++)
+{
+    Console.WriteLine($"Hello {firstNames[i]}");
+}
 
